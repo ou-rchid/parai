e68ba257cf2bc140681c91fd9556de558594fb4f
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2024-07-01 08:19:27 -0500

    display articles in google map

--- wp-content/themes/parai/index.php
+++ wp-content/themes/parai/index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * In this file/page we want to show the "articles post type" in map (e.g., zillow)
  * The main template file. The file for the home page OR the Mapping page in our case.
  *
  * This is the most generic template file in a WordPress theme
@@ -17,80 +18,62 @@
 
 <?php get_header(); ?>
 
-<!-- Container for the map -->
-<h1>Header Goes Here</h1>
+    
 
-<div class="container">
-    <div id="map"></div>
-</div>
-
-<!-- Modal for displaying detailed information -->
-<div id="myModal" class="modal">
-    <span class="close">&times;</span>
-    <div class="modal-content">
-        <div class="modal-header">
-            <h2 id="modal-title"></h2>
-            <span class="close">&times;</span>
-        </div>
-        <div class="modal-body">
-            <p id="modal-description"></p>
-            <div class="grid-container" id="modal-media"></div>
-        </div>
-        <div class="modal-footer">
-            <a href="#" id="modal-link">Read More</a>
-        </div>
-    </div>
-</div>
-
-<!-- this query is for displaying articles in the google map above -->
-<?php
-    // Query to fetch all articles
+<?php    
+    // Query to fetch all articles to show the article in the map
+    // How to query a custom post types: https://developer.wordpress.org/plugins/post-types/working-with-custom-post-types/
     $args = array(
         'post_type' => 'articles',
         'posts_per_page' => -1
     );
-    $articles = new WP_Query($args);
+    
+    $query_articles = new WP_Query($args);
 
-    // Array to store location data for the map
-    $locations = array();
+    // Loop through each article and extract necessary data (title, featured image, location lat long)
+    if ($query_articles->have_posts()) :
+        while ($query_articles->have_posts()) :
+            $query_articles->the_post();
+            //the Lat Long values of each of the article
+            $location = get_post_meta( get_the_ID(), 'location_hardcoded', true );
+            $lat_lng = explode(',', $location);
 
-    // Loop through each article and extract necessary data
-    if ($articles->have_posts()) :
-        while ($articles->have_posts()) :
-            $articles->the_post();
-            $meta = get_post_meta(get_the_ID(), 'parai_location', true);
-            $lat_lng = explode(',', $meta);
             $lat = isset($lat_lng[0]) ? trim($lat_lng[0]) : '';
             $lng = isset($lat_lng[1]) ? trim($lat_lng[1]) : '';
+            
             $lnk = get_permalink(get_the_ID());
             $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail');
-            $description = get_the_excerpt();
+            // $description = get_the_excerpt();
 
-            // Get image URLs
-            $images = array();
-            $image_attachments = get_attached_media('image', get_the_ID());
-            foreach ($image_attachments as $image) {
-                $images[] = wp_get_attachment_url($image->ID);
-            }
+            // Get image URLs [How are these images going to shown in the popup bubble?]
+            // $images = array();
+            // $image_attachments = get_attached_media('image', get_the_ID());
+            // foreach ($image_attachments as $image) {
+            //     $images[] = wp_get_attachment_url($image->ID);
+            // }
 
-            // Get video URLs
-            $videos = array();
-            $video_attachments = get_attached_media('video', get_the_ID());
-            foreach ($video_attachments as $video) {
-                $videos[] = wp_get_attachment_url($video->ID);
-            }
+            // Get video URLs [How are these videos going to shown in the popup bubble?]
+            // $videos = array();
+            // $video_attachments = get_attached_media('video', get_the_ID());
+            // foreach ($video_attachments as $video) {
+            //     $videos[] = wp_get_attachment_url($video->ID);
+            // }
+
+            
+            // Array to store article data (title, location lat long, link, featured image) for the map
+            $articles = array();
 
             // Add location data to the array if both latitude and longitude are available
             if ($lat && $lng) {
-                $locations[] = array(
+                $articles[] = array(
                     'lat' => $lat,
                     'lng' => $lng,
                     'title' => get_the_title(),
                     'link' => $lnk,
                     'thumbnail' => $thumbnail,
-                    'description' => $description,
-                    'images' => implode(',', $images), // comma-separated list of image URLs
-                    'videos' => implode(',', $videos)  // comma-separated list of video URLs
+                    // 'description' => $description,
+                    // 'images' => implode(',', $images), // comma-separated list of image URLs
+                    // 'videos' => implode(',', $videos)  // comma-separated list of video URLs
                 );
             }
         endwhile;
@@ -99,8 +82,20 @@
 ?>
 
 
+<!-- Container for the map -->
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-10"><div id="map"></div></div>
+        <div class="col-sm-2"> List of the Articles will go here </div>
+    </div>
+</div>
+
+
+
+
+
 <!-- the following code will give you the list of articles in the html cards -->
-<?php
+<?php /*
     //In this file we want to show the "articles post type" in map (e.g., zillow)
     // How to query a custom post types: https://developer.wordpress.org/plugins/post-types/working-with-custom-post-types/
 
@@ -137,10 +132,20 @@
 <?php 
 wp_reset_postdata();
     }
+    */
 ?>
 
+
 <script>
-    var locations = <?php echo json_encode($locations); ?>;
+    // this is the latest updates on how to include Google Map API
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+    key: "",
+    v: "weekly",
+    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+    // Add other bootstrap parameters as needed, using camel case.
+  });
+    // this variable holds the articles data that will be displayed in the map
+    var article_locations = <?php echo json_encode($articles); ?>    
 </script>
 
 
--- wp-content/themes/parai/scripts.js
+++ wp-content/themes/parai/scripts.js
@@ -1,85 +1,50 @@
-// Pass PHP array to JavaScript
-// var locations = <?php echo json_encode($locations); ?>;
-console.log("check!");
-// Initialize Google Map
-function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 20.5937, lng: 78.9629 }, // Center map on India
-        zoom: 5 // Set the initial zoom level
+
+// console.log(article_locations); // article_locations is declared in index.php
+
+async function initMap() {
+    // Request needed libraries.
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const map = new Map(document.getElementById("map"), {
+        center: { lat: 13.036218781849394, lng: 77.69639402182948 }, // Center map on India
+        zoom: 7,
+        mapId: "4504f8b37365c3d0",
     });
 
-    // Add markers to the map
-    locations.forEach(function(location) {
-        const marker = new google.maps.Marker({
-            position: { lat: parseFloat(location.lat), lng: parseFloat(location.lng) },
+    
+
+    article_locations.forEach(article_location => {
+
+        // Add marker to the map
+        const marker = new AdvancedMarkerElement({
+            position: { lat: parseFloat(article_location.lat), lng: parseFloat(article_location.lng) },
             map: map,
-            title: location.title
+            title: article_location.title
         });
 
         // Define the content of the info window
         const infoWindowContent = `
-            <div>
-                <img src="${location.thumbnail}" style="max-width: 100%; height: auto; border-radius: 5px;">
-                <h4>${location.title}</h4>
-                <p>${location.description}</p>
-                <a href="#" class="read-more" data-title="${location.title}" data-description="${location.description}" data-images="${location.images}" data-videos="${location.videos}" data-link="${location.link}">Read more</a>
+            <div class="marker-window">
+                <a href="${article_location.link}">
+                    <img src="${article_location.thumbnail}" style="">
+                    <h4>${article_location.title}</h4>
+                </a>
             </div>
         `;
+
+        // add the content to the info window
         const infoWindow = new google.maps.InfoWindow({
             content: infoWindowContent
         });
 
         // Open the info window when the marker is clicked
-        marker.addListener('click', function() {
+        marker.addListener('click', function () {
             infoWindow.open(map, marker);
         });
+
     });
+
 }
 
-// Event listener for "Read more" links to open the modal with detailed info
-document.addEventListener('click', function(e) {
-    if (e.target.classList.contains('read-more')) {
-        e.preventDefault();
+initMap();
 
-        const title = e.target.getAttribute('data-title');
-        const description = e.target.getAttribute('data-description');
-        const images = e.target.getAttribute('data-images').split(',');
-        const videos = e.target.getAttribute('data-videos').split(',');
-        const link = e.target.getAttribute('data-link');
-
-        document.getElementById('modal-title').textContent = title;
-        document.getElementById('modal-description').textContent = description;
-        document.getElementById('modal-link').setAttribute('href', link);
-
-        let modalHTML = '';
-        images.forEach(image => {
-            modalHTML += '<div class="grid-item"><img src="' + image + '" alt="Image" class="modal-image"></div>';
-        });
-        videos.forEach(video => {
-            modalHTML += '<div class="grid-item"><video controls class="modal-video"><source src="' + video + '" type="video/mp4"></video></div>';
-        });
-
-        const modalMedia = document.getElementById('modal-media');
-        modalMedia.innerHTML = modalHTML;
-
-        const modal = document.getElementById('myModal');
-        modal.style.display = 'block';
-    }
-});
-
-// Event listeners to close the modal
-const closeBtns = document.querySelectorAll('.close');
-closeBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-        const modal = document.getElementById('myModal');
-        modal.style.display = 'none';
-    });
-});
-
-// Close the modal when clicking outside of it
-window.addEventListener('click', function(e) {
-    const modal = document.getElementById('myModal');
-    if (e.target === modal) {
-        modal.style.display = 'none';
-    }
-});
