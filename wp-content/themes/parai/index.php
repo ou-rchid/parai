0a752b52a5cb14361a26ad112e9707ec8b66bbf8
Raju Maharjan <36067002+maharrx@users.noreply.github.com>
2024-07-07 23:47:42 -0500

    maps api added

--- wp-content/themes/parai/index.php
+++ wp-content/themes/parai/index.php
@@ -109,11 +109,11 @@
                 /> -->
         
 
-            <div id="map"></div>
+            <div id="map" class=""></div>
         </div>
 
         <div class="col-sm-2"> 
-            <div id="sidebar"></div>
+            <div id="sidebar" class=""></div>
 
 
             <!-- Pagination -->
@@ -149,12 +149,10 @@
 
 <script>
     // this is the latest updates on how to include Google Map API
-//   (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    // key: "",
-    // v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-//   });
+    
+    (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyB8UQmEqkj9DZ4hP5H_cGyGpPYtMtIpIQQ", v: "weekly"});
+
     // this variable holds the articles data that will be displayed in the map
     var article_locations = <?php echo json_encode($articles); ?>    
     // console.log(article_locations);
